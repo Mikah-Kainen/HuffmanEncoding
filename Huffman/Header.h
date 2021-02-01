e188273ec1ffe65dc57a8876f4219f4a21289f9d
Mikah Kainen <mikah@mltd.com>
2021-02-01 14:49:07 -0800

    did

--- Huffman/Header.h
+++ Huffman/Header.h
@@ -8,8 +8,11 @@
 int Main()
 {
 
-
-
+	HeapTree<int> tree = HeapTree<int>();
+	tree.Insert(0);
+	tree.Insert(5);
+	tree.Insert(10);
+	tree.Insert(6);
 
 	return 0;
 }
--- Huffman/HeapTree.h
+++ Huffman/HeapTree.h
@@ -22,20 +22,95 @@ private:
 			return !LeftChild && !RightChild;
 		}
 
-		HeapNode(T value, char letter) : value(value), letter(letter)
+		HeapNode(T value) : value(value)
 		{
 			LeftChild = nullptr;
 			RightChild = nullptr;
 		}
 	};
 
+	int Parent(int index)
+	{
+		return (index - 1 )/ 2;
+	}
+	
+	int LeftChild(int index)
+	{
+		return index * 2 + 1;
+	}
+
+	int RightChild(int index)
+	{
+		return index * 2 + 2;
+	}
+
+	bool Switch(int firstIndex, int secondIndex)
+	{
+		if (firstIndex >= vector.size() || secondIndex >= vector.size())
+		{
+			return false;
+		}
+		HeapNode temp = vector[firstIndex];
+		vector[firstIndex] = vector[secondIndex];
+		vector[secondIndex] = temp;
+		return true;
+	}
+
+	std::vector<HeapNode> vector{};
+
 public:
 	int count = {};
 
-
-	HeapTree()
+	void Insert(T newValue)
 	{
-		
+		vector.push_back(HeapNode(newValue));
+		int newIndex = vector.size() - 1;
+		HeapifyUp(newIndex);
 	}
+
+	void HeapifyUp(int index)
+	{
+		while (index != 0)
+		{
+			if (vector[index] > vector[Parent(index)])
+			{
+				Switch(index, Parent(index));
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+
+	T Pop()
+	{
+		T returnValue = vector[0];
+		vector[0] = vector[vector.size() - 1];
+		vector[vector.size() - 1] = null;
+		HeapifyDown(0);
+		return returnValue;
+	}
+
+	void HeapifyDown(int index)
+	{
+		while (LeftChild(index) < vector.size())
+		{
+			bool isLeftBigger = LeftChild(index) > RightChild(index);
+			if (isLeftBigger)
+			{
+				if (vector[index] < vector[LeftChild(vector)])
+				{
+
+				}
+			}
+			else
+			{
+
+			}
+		}
+	}
+
+
 };
 
